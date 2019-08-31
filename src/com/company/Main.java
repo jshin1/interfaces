b7b5035959e968f4faaa8ba76fd032330bbab8ab
Ji Shin <j2yshin@gmail.com>
2019-08-31 10:04:14 -0400

    Create telephone interface and deskphone class that implements the interface

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ITelephone myPhone;
+        myPhone = new DeskPhone(123456);
+        myPhone.powerOn();
+        myPhone.callPhone(123456);
+        myPhone.answer();
+
+    }
+}
